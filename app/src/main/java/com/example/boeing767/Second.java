e3a884b71c04bd495cdb2ff4f3f70305b07e4f0c
elik <elik@example.com>
2024-02-20 17:52:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/boeing767/Second.java
@@ -0,0 +1,16 @@
+package com.example.boeing767;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+
+import com.example.boeing767.R;
+
+
+public class Second extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
